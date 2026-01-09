2e649c1edd50fc3da2ffb76521b89d9e700d6278
tsuji-108 <atsuhiro-tsuji@gaji.jp>
2026-01-09 11:04:39 +0900

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_07_093439) do
   create_table "todos", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
-    t.string "is_completed"
+    t.boolean "is_completed"
     t.string "name"
     t.datetime "updated_at", null: false
   end
